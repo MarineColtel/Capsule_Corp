3665ddc98d0f5cd01fe42b5ef27b6e98d12f1337
Marine Coltel <marine_coltel@hotmail.fr>
2023-03-20 19:09:01 +0100

    cpasule resa form encore avec js en back
    Signed-off-by: Marine Coltel <marine_coltel@hotmail.fr>

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
     # skiper la protection sur la page home
-  skip_before_action :authenticate_user!, only: :home
+  # skip_before_action :authenticate_user!, only: :home
   def home
 
   end
