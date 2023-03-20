86b76a8a53523e210a06436b112a75830424c88c
Marine Coltel <marine_coltel@hotmail.fr>
2023-03-20 06:52:58 +0100

    gemfile lock updated
    Signed-off-by: Marine Coltel <marine_coltel@hotmail.fr>

--- db/schema.rb
+++ db/schema.rb
@@ -24,4 +24,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_20_134823) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "avatar"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
