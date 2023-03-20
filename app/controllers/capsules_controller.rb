6a242bb47c1d2d23bb9d47b40e3e18f43b18e9c5
HervÃe Nguetsop <herve.nguetsop@gmail.com>
2023-03-20 16:28:30 +0100

    controller routes and views OK
    Signed-off-by: HervÃe Nguetsop <herve.nguetsop@gmail.com>

--- app/controllers/capsules_controller.rb
+++ app/controllers/capsules_controller.rb
@@ -1,4 +1,6 @@
 class CapsulesController < ApplicationController
+  before_action :set_capsule, only: %i[show edit update destroy]
+
   def index
     @capsules = Capsule.all
   end
@@ -17,7 +19,30 @@ class CapsulesController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @capsule.update(capsule_params)
+      redirect_to capsule_path(@capsule)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @capsule.destroy
+    redirect_to capsules_path
+  end
+
   private
+
+  def set_capsule
+    @capsule = Capsule.find(params[:id])
+  end
 
   def capsule_params
     params.require(:capsule).permit(:name, :year, :advices, :price_per_day, photos: [])
