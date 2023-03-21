ffbea71573573ecf7f63738ef91ae38e3a89c062
bechara francis <becharafrancis@gmail.com>
2023-03-21 12:50:23 +0100

    setup_update
    Signed-off-by: bechara francis <becharafrancis@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :bookings
+  has_many :capsule, through: :bookings
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
