2aed7c88ae0958d3c9c3bb664c58ef5b627d7146
gael carayon <gaelcarayon@hotmail.com>
2023-03-21 23:18:24 +0100

    retrait d'une colonne inutile dans booking
    Signed-off-by: gael carayon <gaelcarayon@hotmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_203031) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_21_221134) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,12 +47,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_203031) do
     t.bigint "user_id", null: false
     t.date "start_date"
     t.date "end_date"
-    t.bigint "review_id"
     t.float "total_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["capsule_id"], name: "index_bookings_on_capsule_id"
-    t.index ["review_id"], name: "index_bookings_on_review_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
@@ -94,7 +92,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_203031) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "capsules"
-  add_foreign_key "bookings", "reviews"
   add_foreign_key "bookings", "users"
   add_foreign_key "reviews", "bookings"
 end
