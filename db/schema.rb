49fdec748fa1d811d2334b06357b53f48193a8b4
gael carayon <gaelcarayon@hotmail.com>
2023-03-21 21:50:47 +0100

    correction des liens entre review et booking
    Signed-off-by: gael carayon <gaelcarayon@hotmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_143156) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_21_203031) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,7 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_143156) do
     t.bigint "user_id", null: false
     t.date "start_date"
     t.date "end_date"
-    t.bigint "review_id", null: false
+    t.bigint "review_id"
     t.float "total_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -72,6 +72,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_143156) do
     t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "booking_id", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -94,4 +96,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_143156) do
   add_foreign_key "bookings", "capsules"
   add_foreign_key "bookings", "reviews"
   add_foreign_key "bookings", "users"
+  add_foreign_key "reviews", "bookings"
 end
