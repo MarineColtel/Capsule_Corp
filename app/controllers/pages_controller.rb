c55bfebe35d27b42aa6e04d93517da0340c48491
HervÃe Nguetsop <herve.nguetsop@gmail.com>
2023-03-21 15:59:51 +0100

    update
    Signed-off-by: HervÃe Nguetsop <herve.nguetsop@gmail.com>

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
     # skiper la protection sur la page home
   # skip_before_action :authenticate_user!, only: :home
   def home
-
+    @capsules = Capsule.all
   end
 end
