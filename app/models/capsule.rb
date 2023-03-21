c33d56ba75c67a7f3bf3d2f4a0f8cf086ed12a8d
HervÃe Nguetsop <herve.nguetsop@gmail.com>
2023-03-21 15:06:16 +0100

    update homepage

--- app/models/capsule.rb
+++ app/models/capsule.rb
@@ -1,7 +1,7 @@
 class Capsule < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many_attached :photos
-  belongs_to :user, through: :bookings
+  has_many :user, through: :bookings
 
   validates :name, presence: true
   validates :year, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :bookings
-  has_many :capsule, through: :bookings
+  has_many :capsules, through: :bookings
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
