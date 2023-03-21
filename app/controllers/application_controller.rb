9082779ad56a05d08e6b0cf69796cc7ac5f7373e
bechara francis <becharafrancis@gmail.com>
2023-03-21 18:17:16 +0100

    sign up form
    Signed-off-by: bechara francis <becharafrancis@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # proteger toutes les routes par defaut
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
