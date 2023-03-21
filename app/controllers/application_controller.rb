5b7e3e7885e4c346039aba354291a91e55ace994
bechara francis <becharafrancis@gmail.com>
2023-03-21 19:37:52 +0100

    update_sign_in
    Signed-off-by: bechara francis <becharafrancis@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,8 @@ class ApplicationController < ActionController::Base
 
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
   end
+
+  def after_sign_in_path_for(user)
+    user_path(user)
+  end
 end
