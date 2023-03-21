cfe5d5a5e2fec5178d0e3a111067d6176d5e759d
HervÃe Nguetsop <herve.nguetsop@gmail.com>
2023-03-21 15:38:08 +0100

    update index form
    Signed-off-by: HervÃe Nguetsop <herve.nguetsop@gmail.com>

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
     # skiper la protection sur la page home
-  skip_before_action :authenticate_user!, only: :home
+  #skip_before_action :authenticate_user!, only: :home
   def home
-
+    @capsules = Capsule.all
   end
 end
