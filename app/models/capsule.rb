83ab6d4143fcea3480e6fc06e5f1019e890b64f0
gael carayon <gaelcarayon@hotmail.com>
2023-03-23 17:08:52 +0100

    ajout de la note moyenne dans le show de la capsule
    Signed-off-by: gael carayon <gaelcarayon@hotmail.com>

--- app/models/capsule.rb
+++ app/models/capsule.rb
@@ -1,5 +1,6 @@
 class Capsule < ApplicationRecord
   has_many :bookings, dependent: :destroy
+  has_many :reviews, through: :bookings
   has_one_attached :photo
   belongs_to :user
 
@@ -7,7 +8,7 @@ class Capsule < ApplicationRecord
   validates :year, presence: true
   validates :advices, presence: true
   validates :price_per_day, presence: true
-  
+
   include PgSearch::Model
   pg_search_scope :search_by_name_and_years, against: %i[name year],
                                              using: {
