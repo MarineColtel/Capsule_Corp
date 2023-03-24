2ffdfd7d7a9c3913db2cf2577b5978cb3fa8f365
gael carayon <gaelcarayon@hotmail.com>
2023-03-24 12:56:45 +0100

    fix taille des cartes + boutons suppr eet edit
    Signed-off-by: gael carayon <gaelcarayon@hotmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :bookings
-  has_many :capsules, through: :bookings
+  has_many :capsules
   has_many :reviews, through: :bookings
   has_one_attached :photo
 
